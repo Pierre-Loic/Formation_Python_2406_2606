20675efe55cc16f1759f91ea3b140684ec368af4
Sergio Courtois <courtois.sergio@gmail.com>
2020-06-24 12:29:28 +0200

    Exo1 moyenne

--- exercices/exercice_list.py
+++ exercices/exercice_list.py
@@ -8,7 +8,6 @@
 # - Calculer la moyenne des notes
 
 liste_notes = """
-20
 Emma
 20
 Gabriel
@@ -22,3 +21,19 @@ Raphael
 Léo
 16
 """
+tableau_note_base = liste_notes.split()
+nb_note = len(tableau_note_base)/2
+tableau_note_fin =[]
+var = 0
+for i,j in enumerate(tableau_note_base):
+    if (int(i) % 2) == 0:
+        tableau_note_fin.append([tableau_note_base[int(i)], tableau_note_base[int(i) + 1]])
+        var =  var +  int(tableau_note_base[int(i) + 1])
+
+print("not sorted" + str(tableau_note_fin))
+
+sorted(tableau_note_fin, key=lambda note: note[1])
+
+print("sorted" + str(tableau_note_fin))
+
+print("moyenne" +var/nb_note)
