094f3f3c0d7c9d5b1e7694586efeae5e206162fe
ColinRobert <60734759+ColinRobert@users.noreply.github.com>
2020-06-24 12:07:41 +0200

    Exercice list 1

--- exercices/exercice_list.py
+++ exercices/exercice_list.py
@@ -8,7 +8,6 @@
 # - Calculer la moyenne des notes
 
 liste_notes = """
-20
 Emma
 20
 Gabriel
@@ -22,3 +21,23 @@ Raphael
 Léo
 16
 """
+tableau_1 = liste_notes.split("/n")
+tableau_2 = []
+tableau_3 = []
+sum_note = 0
+nb_note = 0
+
+for i in tableau_1:
+    if i:
+        if i.isdigit():
+            sum_note += int (i)
+            nb_note += 1
+            tableau_3.append(int(i))
+        else:
+            tableau_3 = [i, ]
+            tableau_2.append(tableau_3)
+
+print(tableau_2)
+print(sorted(tableau_2, key=lambda tableau: tableau[1]))
+
+print("Moyenne : " + (sum_note/nb_note))
