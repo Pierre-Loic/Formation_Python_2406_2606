9ed28bdfd4af557b4129b39827424b9aa1478a38
nicolas CHAPELIER <nicolas.chapelier@econocom.com>
2020-06-24 12:05:34 +0200

    exercice 1

--- exercices/exercice_list.py
+++ exercices/exercice_list.py
@@ -8,7 +8,6 @@
 # - Calculer la moyenne des notes
 
 liste_notes = """
-20
 Emma
 20
 Gabriel
@@ -22,3 +21,18 @@ Raphael
 Léo
 16
 """
+#method 1
+listes_1 = []
+for i in range(0, len(liste_notes.split()), 2):
+    listes_1.append([liste_notes.split()[i], liste_notes.split()[i+1]])
+
+listes_1.sort(key = lambda x: x[1])
+
+print(listes_1)
+print(sum(map(int,list(zip(*listes_1))[1]))/len(listes_1))
+
+#method 2
+listes_2 = list(map(list,zip(liste_notes.split()[::2],liste_notes.split()[1::2])))
+listes_2.sort(key = lambda x: x[1])
+print(listes_2)
+print(sum(map(int,list(zip(*listes_1))[1]))/len(listes_1))
