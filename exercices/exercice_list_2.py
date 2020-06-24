85b8355cc0a93b7bc88c39d12ad00bd1068581a5
Couff <thomasmistero@gmail.com>
2020-06-24 11:26:49 +0200

    Exo 2

--- exercices/exercice_list_2.py
+++ exercices/exercice_list_2.py
@@ -5,6 +5,24 @@
 # - Créer une liste vide
 # - Lire les instructions présentées dans la chaine de caractères actions_liste
 # - Appliquer ces actions à la liste vide créée initialement
+def execCommand(cmd,val1,val2):
+    if cmd == "insert":
+        if val2 is not None:
+            empty_list.insert(val1,val2)
+        else:
+            empty_list.insert(val1)
+    elif cmd == "print":
+        print(empty_list)
+    elif cmd == "remove":
+        empty_list.remove(val1)
+    elif cmd == "append":
+        empty_list.append(val1)
+    elif cmd == "sort":
+        empty_list.sort()
+    elif cmd == "pop":
+        empty_list.pop()
+    elif cmd == "reverse":
+        empty_list.reverse()
 
 actions_liste = """
 insert 0 5
@@ -20,3 +38,21 @@ pop
 reverse
 print
 """
+empty_list = []
+actions_liste = actions_liste.split()
+tmp_cmd = ""
+tmp_val_1 = None
+tmp_val_2 = None
+
+for i, val in enumerate(actions_liste):
+    if val.isdigit() == False:
+        execCommand(tmp_cmd,tmp_val_1,tmp_val_2)
+        tmp_cmd = val
+        tmp_val_1 = None
+        tmp_val_2 = None
+    else:
+        if tmp_val_1 is None:
+            tmp_val_1 = int(val)
+        else:
+            tmp_val_2 = int(val)
+execCommand(tmp_cmd,tmp_val_1,tmp_val_2)
