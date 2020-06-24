d984b400d3eabaf3d20bf4ae7402a766c3e03819
Couff <thomasmistero@gmail.com>
2020-06-24 15:31:57 +0200

    enigma

--- exercices/exercice_Enigma.py
+++ exercices/exercice_Enigma.py
@@ -27,8 +27,115 @@
 #
 # Source d'inspiration : Codingame
 
+#---------------------------------------------------
+#-------------------Encodage------------------------
+#---------------------------------------------------
+
+def cesarEncode(message,nombre):
+    newMessage = []
+    for i,val in enumerate(message):
+        iAlphabet = alphabet.index(val)
+        iAlphabet = iAlphabet + nombre
+        iAlphabet = iAlphabet % 26
+        newMessage.append(alphabet[iAlphabet])
+        nombre = nombre +1
+    return newMessage
+
+def encodeRotor(rotor,message):
+    newMessage = []
+    for val in message:
+        iAlphabet = alphabet.index(val)
+        newMessage.append(rotor[iAlphabet])
+    return newMessage
+
+
+
+
+#---------------------------------------------------
+#-------------------Decodage------------------------
+#---------------------------------------------------
+
+def cesarDecode(message,nombre):
+    newMessage = []
+    
+    for i,val in enumerate(message):
+        iAlphabet = alphabet.index(val)
+        iAlphabet = iAlphabet - nombre
+        iAlphabet = iAlphabet % 26
+        newMessage.append(alphabet[iAlphabet])
+        nombre = nombre + 1
+    return newMessage
+
+def decodeRotor(rotor,message):
+    newMessage = []
+    for val in message:
+        iAlphabet = rotor.index(val)
+        newMessage.append(alphabet[iAlphabet])
+    return newMessage
+
+
+
+
+
+#---------------------------------------------------
+#-------------------Programme-----------------------
+#---------------------------------------------------
+
+
+
+
 def encode(rotor1, rotor2, rotor3, message, nombre):
-    pass
+    messageEdit = message.upper()
+    messageEdit = list(messageEdit)
+    print(messageEdit)
+    mes = cesarEncode(messageEdit,nombre)
+    print(mes)
+    mes = encodeRotor(rotor1,mes)
+    print(mes)
+    mes = encodeRotor(rotor2,mes)
+    print(mes)
+    mes = encodeRotor(rotor3,mes)
+    print(mes)
+    return ''.join(mes)
+
 
 def decode(rotor1, rotor2, rotor3, message, nombre):
-    pass
+    messageEdit = message.upper()
+    messageEdit = list(messageEdit)
+    mes = decodeRotor(rotor3,message)
+    print(mes)
+    mes = decodeRotor(rotor2,mes)
+    print(mes)
+    mes = decodeRotor(rotor1,mes)
+    print(mes)
+    mes = cesarDecode(mes,nombre)
+    print(mes)
+    return ''.join(mes)
+
+
+
+
+alphabet = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+listRotor1 = list("BDFHJLCPRTXVZNYEIWGAKMUSQO")
+listRotor2 = list("AJDKSIRUXBLHWTMCQGZNPYFVOE")
+listRotor3 = list("EKMFLGDQVZNTOWYHXUSPAIBRCJ")
+#pas necessaire
+dictRotor1 = dict(zip(alphabet,listRotor1))
+dictRotor2 = dict(zip(alphabet,listRotor2))
+dictRotor3 = dict(zip(alphabet,listRotor3))
+
+message = "ABC"
+decodeMessage = ""
+nombre = 4
+messageEncoder = encode(listRotor1,listRotor2,listRotor3,message,nombre)
+decodeMessage = messageEncoder
+print("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+messageDecoder = decode(listRotor1,listRotor2,listRotor3,decodeMessage,nombre)
+
+
+print("Message de base:" + message)
+print("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+print("Message encoder: " + messageEncoder)
+print("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+print("Message decoder: " + messageDecoder)
+
