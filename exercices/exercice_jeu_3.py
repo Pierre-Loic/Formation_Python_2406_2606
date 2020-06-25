30618fd494ddf264b77c07a9a8864f843c0939e8
__plb__ <plbayart@gmail.com>
2020-06-25 14:57:34 +0200

    update exercie_jeu_3

--- exercices/exercice_jeu_3.py
+++ exercices/exercice_jeu_3.py
@@ -4,4 +4,12 @@
 # ENONCE
 # Le but de l'exercice est d'ajouter des tests unitaires au jeu de morpion
 # pour vérifier le bon fonctionnement du jeu (avec unittest)
+# Les différentes classes créées doivent être testées
+# Les inputs doivent être testés avec des mocks
 
+import unittest
+
+
+
+if __name__ == '__main__':
+    unittest.main()
