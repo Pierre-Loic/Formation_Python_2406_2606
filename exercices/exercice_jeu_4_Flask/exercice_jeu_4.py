ce0117a8120f95ebbcfa17b560157cd7edac76b2
__plb__ <plbayart@gmail.com>
2020-06-26 10:02:46 +0200

    update exercice 4

--- exercices/exercice_jeu_4_Flask/exercice_jeu_4.py
+++ exercices/exercice_jeu_4_Flask/exercice_jeu_4.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def morpion():
     print(request.args.get('number'))
     data = {
         1: "X",
