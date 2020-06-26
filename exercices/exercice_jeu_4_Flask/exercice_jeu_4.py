0f1e016e5808b07a7261ba77861a246ea440e33f
__plb__ <plbayart@gmail.com>
2020-06-26 10:01:22 +0200

    update exercice 4

---
+++ exercices/exercice_jeu_4_Flask/exercice_jeu_4.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    print(request.args.get('number'))
+    data = {
+        1: "X",
+        2: "_",
+        3: "O",
+        4: "X",
+        5: "O",
+        6: "X",
+        7: "_",
+        8: "O",
+        9: "X"
+        }
+    return render_template("main.html", data=data)
+
+app.run()
